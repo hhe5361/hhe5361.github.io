d1e04a84f28c9fa99b3b336c8dc9a96469f0d84a
HyoeunHwang <hhe5361@naver.com>
2026-05-12 02:47:43 +0900

    wrong base address is fixed

--- portfolio-01/vite.config.ts
+++ portfolio-01/vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hhe5361.github.io',
+  base: '/',
   plugins: [
     react({
       babel: {
